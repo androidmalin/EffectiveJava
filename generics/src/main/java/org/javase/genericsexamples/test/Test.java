24f9fa2fb24235f96245407f128b287cae5091f9
androidmalin <malin.myemail@163.com>
2020-06-11 14:51:42 +0800

    add test code

--- generics/src/main/java/org/javase/genericsexamples/test/Test.java
+++ generics/src/main/java/org/javase/genericsexamples/test/Test.java
@@ -6,14 +6,47 @@ import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
-        List<String> strings = new ArrayList<>();
-        unsafeAdd3(strings, "hello");
-        String s = strings.get(0);
-        System.out.println(s);
+//        List<String> strings = new ArrayList<>();
+//        unsafeAdd3(strings, "hello");
+//        String s = strings.get(0);
+//        System.out.println(s);
+
+        test();
+    }
+
+    @SuppressWarnings("rawtypes")
+    private static void test() {
+        Class intClazz = int.class;
+        Class<?> intClazz2 = int.class;
+        Class<Integer> integerClazz = int.class;
+        Class<Integer> integerClazz1 = Integer.class;
+        Class<Number> numberClazz = Number.class;
+        System.out.println("intClazz:" + intClazz.getCanonicalName());
+        System.out.println("intClazz2:" + intClazz2.getCanonicalName());
+        System.out.println("integerClazz:" + integerClazz.getCanonicalName());
+        System.out.println("integerClazz1:" + integerClazz1.getCanonicalName());
+        System.out.println("numberClazz:" + numberClazz.getCanonicalName());
+
+        Class<? extends Number> clazz = int.class;
+    }
+
+    private static void test2() {
+        Building building = new House();
+        Class<Building> buildingClazz = Building.class;
+        Class<House> houseClazz = House.class;
+        House house = houseClazz.cast(building);
+    }
+
+    private static class Building {
+
+    }
+
+    private static class House extends Building {
+
     }
 
     private static void unsafeAdd(List strings, Object object) {
-        strings.add(object);
+        //strings.add(object);
     }
 
     private static void unsafeAdd2(List<Object> strings, Object object) {
