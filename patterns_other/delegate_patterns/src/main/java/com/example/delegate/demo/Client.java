ef0023ad727d874e0dfdf57ac64f2161e9b02d63
androidmalin <malin.myemail@163.com>
2021-02-23 09:47:10 +0800

    modify build.gradle config

--- patterns_other/delegate_patterns/src/main/java/com/example/delegate/demo/Client.java
+++ patterns_other/delegate_patterns/src/main/java/com/example/delegate/demo/Client.java
@@ -1,7 +1,7 @@
 package com.example.delegate.demo;
 
 
-public class Test {
+public class Client {
     public static void main(String[] args) {
         new Boss().command("海报图", new Leader());
         new Boss().command("爬虫", new Leader());
