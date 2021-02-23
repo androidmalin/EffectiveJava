abdaf2dce61ea9ca07309c9569bc90717f20af15
androidmalin <malin.myemail@163.com>
2021-02-23 10:07:19 +0800

    adjust the directory structure

--- javabase/uml/src/main/java/com/example/uml/aggregate/Hat.java
+++ javabase/uml/src/main/java/com/example/uml/aggregate/Hat.java
@@ -1,6 +1,5 @@
 package com.example.uml.aggregate;
 
-
 public class Hat {
     private String name;
 }
--- patterns_not_gof23/delegate_patterns/src/main/java/com/example/delegate/demo/Leader.java
+++ patterns_not_gof23/delegate_patterns/src/main/java/com/example/delegate/demo/Leader.java
@@ -3,7 +3,6 @@ package com.example.delegate.demo;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Leader implements IEmployee {
 
     private final Map<String, IEmployee> employee = new HashMap<>();
@@ -13,6 +12,7 @@ public class Leader implements IEmployee {
         employee.put("海报图", new EmployeeB());
     }
 
+    @Override
     public void doing(String task) {
         if (!employee.containsKey(task)) {
             System.out.println("这个任务" + task + "超出我的能力范围");
