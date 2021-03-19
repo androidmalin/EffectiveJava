b6dba3d47516030ce73b9f3abc19bbcb57ee54a2
androidmalin <malin.myemail@163.com>
2021-03-19 10:23:46 +0800

    upgrade the version of agp and add note

--- algorithm_practice/src/main/java/com/example/practice/tree/Tree_BFS.java
+++ algorithm_practice/src/main/java/com/example/practice/tree/Tree_BFS.java
@@ -14,31 +14,35 @@ import java.util.Queue;
  * 循环和递归的区别和联系
  * https://blog.csdn.net/Beyond_2016/article/details/81283054
  * <p>
- * 递归（recursion）：在函数内调用自身，将复杂情况逐步转化成基本情况。
- * 循环（loop）：在满足条件的情况下，重复执行同一段代码，比如while语句
+ * 递归（recursion）：在函数内调用自身,将复杂情况逐步转化成基本情况。
+ * 循环（loop）：在满足条件的情况下,重复执行同一段代码,比如while语句
  */
 public class Tree_BFS {
     public static void main(String[] args) {
         TreeCreateFactory.init(Arrays.asList(1, 2, 3, 4, 5, 6, null));
         TreeNode root = TreeCreateFactory.getRootNode();
+        System.out.println("level ");
         printLine(root);
+        System.out.println(" ");
         System.out.println("levelOrder");
         levelOrder(root);
-        System.out.println("traversal");
-        bfs_recursion_traversal(root);
+        System.out.println(" ");
+        System.out.println("bfs_recursion_traversal");
+        recursion_traversal(root);
+        System.out.println(" ");
     }
 
 
     /**
-     * 广度优先是一种横向的获取方式，先从树的较浅层开始获取节点，
+     * 广度优先是一种横向的获取方式,先从树的较浅层开始获取节点,
      * 直接获取完同层次的所有页面后才进入下一层。
-     * 层次遍历是广度优先遍历，层次遍历是二叉树按照从根节点到叶子节点的层次关系，
+     * 层次遍历是广度优先遍历,层次遍历是二叉树按照从根节点到叶子节点的层次关系,
      * 一层一层横向遍历各个节点。
      * <p>
      * 思路
-     * 层次遍历的非递归借助了队列实现，队列的特点是先进先出。
-     * 首先访问根节点并进队，队列不为空时出队并输出节点的值，
-     * 如果当前出队节点有左孩子则左孩子先进队，如果有右孩子则右孩子后进队
+     * 层次遍历的非递归借助了队列实现,队列的特点是先进先出。
+     * 首先访问根节点并进队,队列不为空时出队并输出节点的值,
+     * 如果当前出队节点有左孩子则左孩子先进队,如果有右孩子则右孩子后进队
      */
     private static void printLine(TreeNode rootNode) {
 
@@ -77,6 +81,8 @@ public class Tree_BFS {
     /**
      * 广度优先遍历
      * BFS + 非递归写法
+     * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/die-dai-di-gui-duo-tu-yan-shi-102er-cha-shu-de-cen/
+     * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/bfs-de-shi-yong-chang-jing-zong-jie-ceng-xu-bian-l/
      */
     private static List<List<Integer>> levelOrder(TreeNode rootNode) {
 
@@ -118,32 +124,32 @@ public class Tree_BFS {
 
 
     /**
-     * 广度优先遍历
-     * BFS + 递归
+     * 递归实现 层次遍历
+     * 深度优先遍历.
+     * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/solution/die-dai-di-gui-duo-tu-yan-shi-102er-cha-shu-de-cen/
      */
-    private static void bfs_recursion_traversal(TreeNode root) {
-        // 每一层都要借助一个 List 存储元素
-        List<List<Integer>> resultList = new ArrayList<>();
-        find(resultList, 0, root);
-        System.out.println("递归");
-        System.out.println(resultList);
+    public static List<List<Integer>> recursion_traversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        //用来存放最终结果
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(res, 1, root);
+        System.out.println(res);
+        return res;
     }
 
-
-    /**
-     * 将第 level 层的 node 节点放入 res 的第 level 个集合中
-     */
-    private static void find(List<List<Integer>> res, int level, TreeNode node) {
-        if (node == null) return;
-        if (res.size() <= level) {
-            List<Integer> list = new ArrayList<>();
-            list.add(node.val);
-            res.add(list);
-        } else {
-            List<Integer> list = res.get(level);
-            list.add(node.val);
+    public static void dfs(List<List<Integer>> res, int index, TreeNode root) {
+        if (root == null) return;
+        //假设res是[ [1],[2,3] ], index是3,就再插入一个空list放到res中
+        if (res.size() < index) {
+            res.add(new ArrayList<>());
         }
-        find(res, level + 1, node.left);
-        find(res, level + 1, node.right);
+        //将当前节点的值加入到res中,index代表当前层,假设index是3,节点值是99
+        //res是[ [1],[2,3] [4] ],加入后res就变为 [ [1],[2,3] [4,99] ]
+        res.get(index - 1).add(root.val);
+        //递归的处理左子树,右子树,同时将层数index+1
+        dfs(res, index + 1, root.left);
+        dfs(res, index + 1, root.right);
     }
 }
